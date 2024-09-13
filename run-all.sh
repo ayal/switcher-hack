54def74e883bcdd21eb99ede2718f3df72ffcc18
ayal <ayalg@wix.com>
2024-09-13 10:45:36 +0300

    restart

--- run-all.sh
+++ run-all.sh
@@ -1,3 +1,4 @@
+cd /home/ayalg/switcher-hack
 nohup python -u auto.py > auto-log.log 2>&1 &
 nohup python -u server.py > server-log.log 2>&1 &
 nohup cloudflared tunnel run --url localhost:3001 my-tunnel > tunnel-log.log 2>&1 &
